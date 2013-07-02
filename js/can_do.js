6026e5f46fd88a6453fd0393cb12b8d67970ef95
weslleymberg <weslleym.lisboa@gmail.com>
2013-07-02 17:51:50 -0300

    Enables 'enter button' submit

--- js/can_do.js
+++ js/can_do.js
@@ -6,4 +6,15 @@ $(function(){
 			$("div.tasks").append('<div><span>'+title+'</span></div>');
 		}
 	})
+
+	$("input.new_task").keypress(function(event) {
+		if (event.which == 13) {
+			event.preventDefault();
+			var title = $("input.new_task").val();
+			if (title!="") {
+				$("input.new_task").get(0).value="";
+				$("div.tasks").append('<div><span>'+title+'</span></div>');
+			}
+		}
+	})
 })
