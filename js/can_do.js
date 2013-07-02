96e0adee6cdd11ae4acf132e17d057b771bb7218
weslleymberg <weslleym.lisboa@gmail.com>
2013-07-02 18:11:41 -0300

    Implements task counter

--- js/can_do.js
+++ js/can_do.js
@@ -1,20 +1,25 @@
 $(function(){
-	$("input.bt_create").click(function(){
-		var title = $("input.new_task").val();
+	var title = "";
+	var count = 0;
+
+	function add_task(title) {
 		if (title!="") {
 			$("input.new_task").get(0).value="";
 			$("div.tasks").append('<div><span>'+title+'</span></div>');
+			$("div.footer span.counter").text("Tasks: "+(count+=1));
 		}
+	}
+
+	$("input.bt_create").click(function(){
+		title = $("input.new_task").val();
+		add_task(title);
 	})
 
 	$("input.new_task").keypress(function(event) {
 		if (event.which == 13) {
 			event.preventDefault();
-			var title = $("input.new_task").val();
-			if (title!="") {
-				$("input.new_task").get(0).value="";
-				$("div.tasks").append('<div><span>'+title+'</span></div>');
-			}
+			title = $("input.new_task").val();
+			add_task(title);
 		}
 	})
 })
