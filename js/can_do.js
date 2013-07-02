01b77f8ad75db3cce5b7dd44c75f413b829b021b
weslleymberg <weslleym.lisboa@gmail.com>
2013-07-02 01:11:52 -0300

    Adds action to task button and makes design more pomodoro like

---
+++ js/can_do.js
@@ -0,0 +1,9 @@
+$(function(){
+	$("input.bt_create").click(function(){
+		var title = $("input.new_task").val();
+		if (title!="") {
+			$("input.new_task").get(0).value="";
+			$("div.tasks").append('<div>'+title+'</div>');
+		}
+	})
+})
