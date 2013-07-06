cbe89b86f662c4fba7a6de2dab150b636ea21f3b
weslleymberg <weslleym.lisboa@gmail.com>
2013-07-06 11:07:39 -0300

    Enables swipe left event for tasks deleting

--- js/can_do.js
+++ js/can_do.js
@@ -1,25 +1,49 @@
+(function ($) {
+$.fn.disableSelection = function () {
+    return this.each(function () {
+        if (typeof this.onselectstart != 'undefined') {
+            this.onselectstart = function() { return false; };
+        } else if (typeof this.style.MozUserSelect != 'undefined') {
+            this.style.MozUserSelect = 'none';
+        } else {
+            this.onmousedown = function() { return false; };
+        }
+    });
+};
+})(Zepto);
+
 $(function(){
 	var title = "";
 	var count = 0;
 
+	$("body").disableSelection();
+
 	function add_task(title) {
 		if (title!="") {
 			$("input.new_task").get(0).value="";
-			$("div.tasks").append('<div><span>'+title+'</span></div>');
+			$("div.tasks").append('<div class=swipe><span>'+title+'</span></div>');
 			$("div.footer span.counter").text("Tasks: "+(count+=1));
 		}
 	}
 
-	$("input.bt_create").click(function(){
+	$("div.tasks").on('swipeRight', ".swipe", function() {
+		$(this).animate({'margin-left':$(window).width(), 
+						 'margin-right':-$(window).width()}, 300, 'linear', function() {
+			$(this).remove();
+		})
+		$("div.footer span.counter").text("Tasks: "+(count-=1));
+		console.log(this, event);
+	})
+
+	$("input.bt_create").tap(function(event){
 		title = $("input.new_task").val();
 		add_task(title);
-	})
+	});
 
 	$("input.new_task").keypress(function(event) {
 		if (event.which == 13) {
-			event.preventDefault();
 			title = $("input.new_task").val();
 			add_task(title);
 		}
-	})
-})
+	});
+});
