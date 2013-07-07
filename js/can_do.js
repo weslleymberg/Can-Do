c806dbcb746f46820028404d3f51fee2dcedae4b
weslleymberg <weslleym.lisboa@gmail.com>
2013-07-07 00:13:16 -0300

    Tasks now can be moved from one list to another

--- js/can_do.js
+++ js/can_do.js
@@ -1,41 +1,29 @@
-(function ($) {
-$.fn.disableSelection = function () {
-    return this.each(function () {
-        if (typeof this.onselectstart != 'undefined') {
-            this.onselectstart = function() { return false; };
-        } else if (typeof this.style.MozUserSelect != 'undefined') {
-            this.style.MozUserSelect = 'none';
-        } else {
-            this.onmousedown = function() { return false; };
-        }
-    });
-};
-})(Zepto);
-
 $(function(){
 	var title = "";
-	var count = 0;
+	var bt_event = "click";
 
 	$("body").disableSelection();
 
 	function add_task(title) {
 		if (title!="") {
 			$("input.new_task").get(0).value="";
-			$("ul#inventory").append('<li class=\"swipe open\"><span>'+title+'</span></li>');
-			$("div.footer span.counter").text("Tasks: "+(count+=1));
+			$("ul#inventory").append('<li class=\"swipe move open\"><input type="button" class="bt_move"><span>'+title+'</span></li>');
+			$("input.new_task").blur();
 		}
 	}
 
-	$("ul.task_list").on('swipeRight', "li.swipe", function() {
-		$(this).animate({'margin-left':$(window).width(), 
-						 'margin-right':-$(window).width()}, 300, 'linear', function() {
-			$(this).remove();
-		})
-		$("div.footer span.counter").text("Tasks: "+(count-=1));
-		console.log(this, event);
-	})
+	if (Zepto.browser.mobile) {
+		$("ul.task_container").on('swipeRight', "li.swipe", function() {
+			$(this).animate({'margin-left':$(window).width(), 
+							 'margin-right':-$(window).width()}, 200, 'linear', function() {
+				$(this).remove();
+			})
+		});	
 
-	$("input.bt_create").tap(function(event){
+		bt_event = "tap";
+	}
+
+	$("input.bt_create").on(bt_event, function(event){
 		title = $("input.new_task").val();
 		add_task(title);
 	});
@@ -46,4 +34,18 @@ $(function(){
 			add_task(title);
 		}
 	});
+
+	$("ul.task_container").on(bt_event, "input.bt_move", function(event) {
+		var location = $(this).closest(".move").closest(".task_container");
+		if (location.attr("id") == "inventory") {
+			$(this).closest(".move").removeClass("open");
+			$(this).closest(".move").addClass("ongoing");
+			$("ul#today").append($(this).closest(".move"));
+		}
+		if (location.attr("id") == "today") {
+			$(this).closest(".move").removeClass("ongoing");
+			$(this).closest(".move").addClass("open");
+			$("ul#inventory").append($(this).closest(".move"));
+		}
+	})
 });
